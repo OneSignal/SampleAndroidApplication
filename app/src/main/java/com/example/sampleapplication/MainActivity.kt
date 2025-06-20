69ecd5f727b1696ec5c8c7d089791339b876ae90
Nan <nan@onesignal.com>
2025-06-20 14:34:22 -0700

    Add stuff

--- app/src/main/java/com/example/sampleapplication/MainActivity.kt
+++ app/src/main/java/com/example/sampleapplication/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.example.sampleapplication
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody.Companion.toRequestBody
+import okhttp3.Response
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
+    private val client = OkHttpClient()
+    private var txtString: TextView? = null
+    private var button: Button? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +29,58 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    /**
+     * Example of a GET HTTP request using OkHttp
+     */
+    fun getExample() {
+        val request = Request.Builder()
+            .url("https://publicobject.com/helloworld.txt")
+            .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) { }
+
+            override fun onResponse(call: Call, response: Response) {
+                for ((name, value) in response.headers) {
+                    println("$name: $value")
+                }
+                val responseString = response.body!!.string()
+                println(responseString)
+            }
+        })
+    }
+
+    /**
+     * Example of a Patch HTTP request using OkHttp
+     */
+    fun patchExample() {
+        val payload = """
+            {
+                "name": "alice",
+                "properties": {
+                    "country": "US",
+                    "language": "en"
+                }
+            }
+        """.trimIndent()
+
+        val request = Request.Builder()
+            .url("https://api.github.com/markdown/raw")
+            .patch(payload.toRequestBody())
+            .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) { }
+
+            override fun onResponse(call: Call, response: Response) {
+                for ((name, value) in response.headers) {
+                    println("$name: $value")
+                }
+                val responseString = response.body!!.string()
+                println(responseString)
+            }
+        })
     }
 }
